796820b9a0fa8a2ffba9f9cd3ca05166b9fdd115
mabgcm <bugucam@gmail.com>
2023-12-09 16:24:25 -0500

    rollback 3

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import BookingList from '../../components/BookingList'
-import UserInfo from '../../components/UserInfo'
+// import BookingList from '../../components/BookingList'
+// import UserInfo from '../../components/UserInfo'
 
 const Dashboard = () => {
   return (
