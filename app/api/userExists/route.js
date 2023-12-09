4e54c9d863cd1182b0d9c9bb6304fbc7d7f88fbb
mabgcm <bugucam@gmail.com>
2023-12-09 17:06:45 -0500

    import routes

--- app/api/userExists/route.js
+++ app/api/userExists/route.js
@@ -1,5 +1,5 @@
-import { connectMongoDB } from "@/app/lib/mongodb";
-import User from "@/models/user";
+import { connectMongoDB } from "../../lib/mongodb";
+import User from "../../models/users";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
