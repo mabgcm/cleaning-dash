c8711670fa372e0e7a14fcad011e368f2ed0e890
mabgcm <bugucam@gmail.com>
2023-12-09 16:45:54 -0500

    models and libs added

---
+++ models/booking.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from 'mongoose';
+
+const bookingSchema = new Schema({
+    name: String,
+    phone: Number,
+    email: String,
+    adress: String,
+    city: String,
+    postalCode: String,
+    bedrooms: Number,
+    bathrooms: Number,
+    squareFeetRange: String,
+    cleaningItems: [String],
+    totalAmount: Number
+},
+    {
+        timestamps: true
+    });
+
+const Booking = mongoose.models.Booking || mongoose.model('Booking', bookingSchema);
+
+export default Booking;
