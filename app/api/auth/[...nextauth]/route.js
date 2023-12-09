fc2c98487f2b7b917c975f76a50ab49abb9eba27
mabgcm <bugucam@gmail.com>
2023-12-09 17:02:33 -0500

    import routes fixed

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,5 @@
-import { connectMongoDB } from "@/app/lib/mongodb";
-import User from "@/models/user";
+import { connectMongoDB } from "../../../lib/mongodb";
+import User from "../../../models/users";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
