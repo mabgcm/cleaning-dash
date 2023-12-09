59c8b1d412ac7058402983fc29b3b0783f2a0dd5
mabgcm <bugucam@gmail.com>
2023-12-09 17:12:05 -0500

    api/bookings added

---
+++ app/api/bookings/route.js
@@ -0,0 +1,24 @@
+import connectMongoDB from "../../lib/db";
+import Booking from "../../models/booking";
+import { NextResponse } from 'next/server'
+
+
+export async function POST(request) {
+    const { name, phone, email, adress, city, postalCode, bedrooms, bathrooms, squareFeetRange, cleaningItems, totalAmount } = await request.json();
+    await connectMongoDB();
+    await Booking.create({ name, phone, email, adress, city, postalCode, bedrooms, bathrooms, squareFeetRange, cleaningItems, totalAmount });
+    return NextResponse.json({ message: "Booking created" }, { status: 201 })
+}
+
+export async function GET() {
+    await connectMongoDB();
+    const bookings = await Booking.find();
+    return NextResponse.json({ bookings });
+}
+
+export async function DELETE(request) {
+    const id = request.nextUrl.searchParams.get("id");
+    await connectMongoDB();
+    await Booking.findByIdAndDelete(id);
+    return NextResponse.json({ message: "Booking deleted" }, { status: 200 });
+}
