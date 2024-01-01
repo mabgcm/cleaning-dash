d2318940176c6317260aca0fe7df1790599cdfc1
mabgcm <bugucam@gmail.com>
2024-01-01 12:31:53 -0500

    month fixed

--- components/TotalAmount.jsx
+++ components/TotalAmount.jsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useEffect, useState } from 'react';
-import { parse } from 'date-fns';
+import { parse, format } from 'date-fns';
 
 
 const TotalAmount = ({ bookings }) => {
