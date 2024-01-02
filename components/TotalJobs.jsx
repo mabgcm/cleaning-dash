bfce31125278c766e5d305c7181a8a91f49159f7
mabgcm <bugucam@gmail.com>
2024-01-02 07:11:53 -0500

    rollback to total jobs for stract

--- components/TotalJobs.jsx
+++ components/TotalJobs.jsx
@@ -1,47 +1,26 @@
 'use client'
 import React, { useEffect, useState } from 'react';
-import { format, isThisWeek, isThisMonth, isThisYear } from 'date-fns';
 
-const TotalJobs = ({ bookings }) => {
-    const [jobsThisWeek, setJobsThisWeek] = useState(0);
-    const [jobsThisMonth, setJobsThisMonth] = useState(0);
-    const [jobsThisYear, setJobsThisYear] = useState(0);
+const TotalJobsDone = ({ bookings }) => {
+    const [totalJobsDone, setTotalJobsDone] = useState(0);
 
     useEffect(() => {
-        const calculateTotalJobs = () => {
-            if (bookings.length === 0) {
-                // If bookings is empty, return early
-                return;
-            }
-
-            const today = new Date();
-
-            const weeklyJobs = bookings.filter((booking) => isThisWeek(new Date(booking.date), { weekStartsOn: 1 }));
-            const monthlyJobs = bookings.filter((booking) => isThisMonth(new Date(booking.date)));
-            const yearlyJobs = bookings.filter((booking) => isThisYear(new Date(booking.date)));
-
-            setJobsThisWeek(weeklyJobs.length);
-            setJobsThisMonth(monthlyJobs.length);
-            setJobsThisYear(yearlyJobs.length);
+        const calculateTotalJobsDone = () => {
+            const doneBookings = bookings.filter((booking) => booking.completed);
+            setTotalJobsDone(doneBookings.length);
         };
 
-        calculateTotalJobs();
-    }, [bookings]); // Make sure to include bookings as a dependency
+        calculateTotalJobsDone();
+    }, [bookings]);
 
     return (
         <div className="p-4 border border-slate-300">
-            <h4>Total Jobs:</h4>
+            <h4>Total Jobs Done:</h4>
             <div>
-                <strong>This Week ({format(new Date(), 'MMM dd, yyyy')}):</strong> {jobsThisWeek}
-            </div>
-            <div>
-                <strong>This Month ({format(new Date(), 'MMMM yyyy')}):</strong> {jobsThisMonth}
-            </div>
-            <div>
-                <strong>This Year ({format(new Date(), 'yyyy')}):</strong> {jobsThisYear}
+                <strong>{totalJobsDone}</strong>
             </div>
         </div>
     );
 };
 
-export default TotalJobs;
+export default TotalJobsDone;
