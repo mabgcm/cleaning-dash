b549f2df9276e28364c5d64897f4f67d73436798
mabgcm <bugucam@gmail.com>
2024-01-08 16:14:49 -0500

    mb-10

--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -66,7 +66,7 @@ export default function DrawerAppBar(props) {
   const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex', mb: 100 }}>
+    <Box sx={{ display: 'flex', mb: 10 }}>
       <CssBaseline />
       <AppBar component="nav">
         <Toolbar>
