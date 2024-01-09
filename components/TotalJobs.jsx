6f62e9f1b0078ce6c256eb5c088555a66934bbb6
mabgcm <bugucam@gmail.com>
2024-01-09 15:24:20 -0500

    flx drc col

--- components/TotalJobs.jsx
+++ components/TotalJobs.jsx
@@ -50,7 +50,7 @@ const TotalJobs = ({ bookings }) => {
                 {/* <h4>Total Jobs:</h4> */}
             </div>
             <div>
-                <Box sx={{ color: '#fff', alignItems: 'center', display: 'flex', flexDirection: 'row', }}>
+                <Box sx={{ color: '#fff', alignItems: 'center', display: 'flex', flexDirection: 'column', }}>
                     <Typography sx={{ fontSize: 20, fontWeight: 'bold' }}>{totalJobs}</Typography>
                     <Typography sx={{ fontSize: 12 }}>Total Jobs</Typography>
 
