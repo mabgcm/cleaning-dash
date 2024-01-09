8f6029a290af642615cf69cc17be9dd91e2560a0
mabgcm <bugucam@gmail.com>
2024-01-09 14:31:58 -0500

    total jobs

--- components/TotalJobs.jsx
+++ components/TotalJobs.jsx
@@ -1,106 +1,41 @@
-'use client';
+'use client'
 import React, { useEffect, useState } from 'react';
 import Card from '@mui/material/Card';
-import {
-    startOfDay,
-    startOfWeek,
-    endOfWeek,
-    startOfMonth,
-    endOfMonth,
-    isWithinInterval as isWithinIntervalFn, // Rename the custom function
-    isSameDay
-} from 'date-fns';
+import Box from '@mui/material/Box';
 
 const TotalJobs = ({ bookings }) => {
-    const [totalJobsDone, setTotalJobsDone] = useState({ today: 0, week: 0, month: 0 });
-    const [totalJobsLeft, setTotalJobsLeft] = useState({ today: 0, week: 0, month: 0 });
+    const [totalJobs, setTotalJobs] = useState(0);
+
+    // Function to calculate total number of jobs
+    const calculateTotalJobs = (bookings) => {
+        let total = 0;
+
+        bookings.forEach((booking) => {
+            if (booking.paid) {
+                total += 1;
+            }
+        });
+
+        setTotalJobs(total);
+        return total;
+    };
 
     useEffect(() => {
-        console.log('Bookings in TotalJobs component:', bookings);
-
-        const calculateTotalJobs = () => {
-            const currentDate = new Date();
-            const startOfToday = startOfDay(currentDate);
-            const startOfThisWeek = startOfWeek(currentDate);
-            const endOfThisWeek = endOfWeek(currentDate);
-            const startOfThisMonth = startOfMonth(currentDate);
-            const endOfThisMonth = endOfMonth(currentDate);
-
-            console.log('Current Date:', currentDate);
-            console.log('Start of Today:', startOfToday);
-            console.log('Start of This Week:', startOfThisWeek);
-            console.log('End of This Week:', endOfThisWeek);
-            console.log('Start of This Month:', startOfThisMonth);
-            console.log('End of This Month:', endOfThisMonth);
-
-
-            const todayBookings = bookings.filter((booking) => {
-                const bookingDate = new Date(booking.date);
-                return booking.completed && isSameDay(bookingDate, currentDate);
-            });
-
-            console.log('Today Bookings:', todayBookings);
-
-
-            const weekBookings = bookings.filter((booking) => {
-                const bookingDate = new Date(booking.date);
-                return booking.completed && isWithinIntervalFn(bookingDate, { start: startOfThisWeek, end: endOfThisWeek });
-            });
-
-            console.log('This Week Bookings:', weekBookings);
-
-
-            const monthBookings = bookings.filter((booking) => {
-                const bookingDate = new Date(booking.date);
-                return booking.completed && isWithinIntervalFn(bookingDate, { start: startOfThisMonth, end: endOfThisMonth });
-            });
-
-            console.log('This Month Bookings:', monthBookings);
-
-
-            setTotalJobsDone({
-                today: todayBookings.length,
-                week: weekBookings.length,
-                month: monthBookings.length,
-            });
-
-            const remainingToday = bookings.filter((booking) => !booking.completed && isSameDay(new Date(booking.date), currentDate)).length;
-            const remainingWeek = bookings.filter((booking) => !booking.completed && isWithinIntervalFn(new Date(booking.date), { start: startOfThisWeek, end: endOfThisWeek })).length;
-            const remainingMonth = bookings.filter((booking) => !booking.completed && isWithinIntervalFn(new Date(booking.date), { start: startOfThisMonth, end: endOfThisMonth })).length;
-
-            console.log('Remaining Today:', remainingToday);
-            console.log('Remaining Week:', remainingWeek);
-            console.log('Remaining Month:', remainingMonth);
-
-            setTotalJobsLeft({
-                today: remainingToday,
-                week: remainingWeek,
-                month: remainingMonth,
-            });
-        };
-
-        calculateTotalJobs();
+        const total = calculateTotalJobs(bookings);
+        setTotalJobs(total);
     }, [bookings]);
 
     return (
-        <Card sx={{ height: 200 }} className="p-4 border border-slate-300">
+        <Card
+            sx={{ height: 200, bgcolor: 'primary.main' }}
+            className="p-4 border border-slate-300">
             <div>
-                <p><strong>{totalJobsDone.today}</strong> jobs done today</p>
+                {/* <h4>Total Jobs:</h4> */}
             </div>
             <div>
-                <p><strong>{totalJobsLeft.today}</strong> jobs left today</p>
-            </div>
-            <div>
-                <p><strong>{totalJobsDone.week}</strong> jobs done this week</p>
-            </div>
-            <div>
-                <p><strong>{totalJobsLeft.week}</strong> jobs left this week</p>
-            </div>
-            <div>
-                <p><strong>{totalJobsDone.month}</strong> jobs done this month</p>
-            </div>
-            <div>
-                <p><strong>{totalJobsLeft.month}</strong> jobs left this month</p>
+                <Box sx={{ color: '#fff', fontSize: 26, fontWeight: 'bold' }}>
+                    <p>{totalJobs}</p>
+                </Box>
             </div>
         </Card>
     );
